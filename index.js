8ced8d23e9723f2b7c6eed0c961616add40a7fb3
antoine.leveugle <antoine.leveugle@gmail.com>
2017-01-02 11:40:08 +0100

    feat(slack): export SlackService

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.VirtualAssistant = require('./src/virtual-assistant');
 module.exports.AssistantFeature = require('./src/feature/assistant-feature');
 module.exports.ConfigurationService = require('./src/helpers/configuration-service');
+module.exports.SlackService = require('./src/interface/slack-service');
