d767e031be359dba16f01ab451ea71c781116fc8
antoine.leveugle <antoine.leveugle@gmail.com>
2017-01-28 16:00:31 +0100

    feat: expose DatabaseService

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports.VirtualAssistant = require('./src/virtual-assistant');
 module.exports.AssistantFeature = require('./src/feature/assistant-feature');
 module.exports.ConfigurationService = require('./src/helpers/configuration-service');
+module.exports.DatabaseService = require('./src/helpers/database-service');
 module.exports.SlackService = require('./src/interface/slack-service');
