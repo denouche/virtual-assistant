4e7fc63349c0bcf91ec3948c6fd0a3f8a7f0a5f7
Ludovic Borie <ludovic@bodul.fr>
2017-01-30 22:31:05 +0100

    feat(database): change all the persistence to use js-data, add Google Datastore adapter and MongoDB adapter

--- src/helpers/database/mongodb.js
+++ src/helpers/database/mongodb.js
@@ -1,119 +1,43 @@
-const AbstractDAO = require('./abstract-dao'),
-    MongoClient = require('mongodb').MongoClient,
-    ConfigurationService = require('../configuration-service'),
-    debug = require('debug')('virtual-assistant:database-mongodb');
+const ConfigurationService = require('../configuration-service'),
+    debug = require('debug')('virtual-assistant:database-mongodb'),
+    MongoDBAdapter = require('js-data-mongodb'),
+    Container = require('js-data').Container;
 
-class MongodbDAO extends AbstractDAO {
 
-    static _getCollection(name) {
+class MongodbDao {
+
+    static getContainer(name) {
         if(!this.dbUrl) {
             this.dbUrl = ConfigurationService.get('database.mongodb.url');
             if(!this.dbUrl) {
-                debug('Error: missing mongodb configuration URL. Please set the configuration `database.mongodb.url` with the database url (eg: `mongodb://localhost:27017/myproject`)');
-                return null;
+                debug(`Error: missing mongodb configuration URL.
+                    Please set the configuration \`database.mongodb.url\` with the database url (eg: \`mongodb://localhost:27017/myproject\`)
+                    Also be careful to encodeURIComponent the password if it contains special chars`);
+            }
+            else {
+                this.dbUrl = this.dbUrl.replace(/^<(.*)>$/, '$1'); // strip wrong chars if the url has a link form
             }
         }
-    	if(!this.db) {
-    		this.db = new Promise((resolve, reject) => {
-	    		MongoClient.connect(this.dbUrl, function(err, db) {
-	    			if(err) {
-	    				reject(err)
-	    			}
-	    			else {
-		    			resolve(db);
-		    		}
-	    		});
-    		});
-    	}
-		return this.db.then(function(db) {
-			return db.collection(name);
-		});
-    }
 
-    insertOne(doc) {
-    	var args = arguments;
-    	return this.constructor._getCollection(this.collection)
-    		.then(function(collection) {
-    			return collection.insertOne.apply(collection, args);
-    		});
-    }
+        let adapter;
+        if(this.adapter) {
+            adapter = this.adapter;
+        }
+        else {
+            adapter = new MongoDBAdapter({
+              uri: this.dbUrl
+            });
+            if(this.dbUrl) {
+                // Only keep adapter if the url is set, to avoid reloading on first configuration
+                this.adapter = adapter;
+            }
+        }
 
-    insertMany(docs) {
-    	var args = arguments;
-    	return this.constructor._getCollection(this.collection)
-    		.then(function(collection) {
-    			return collection.insertMany.apply(collection, args);
-    		});
-    }
-
-    find(query) {
-    	var args = arguments;
-    	return this.constructor._getCollection(this.collection)
-    		.then(function(collection) {
-    			return collection.find.apply(collection, args);
-    		})
-    		.then(function(results) {
-    			return new Promise((resolve, reject) => {
-    				results.toArray(function(err, documents) {
-    					if(err) {
-    						reject(err);
-    					}
-    					else {
-    						resolve(documents);
-    					}
-    				});
-    			});
-    		});
-    }
-
-    findOne(query) {
-    	var args = arguments;
-    	return this.constructor._getCollection(this.collection)
-    		.then(function(collection) {
-    			return collection.findOne.apply(collection, args);
-    		});
-    }
-
-    count(query) {
-    	var args = arguments;
-    	return this.constructor._getCollection(this.collection)
-    		.then(function(collection) {
-    			return collection.count.apply(collection, args);
-    		});
-    }
-
-    updateOne(query, update, options) {
-    	var args = arguments;
-    	return this.constructor._getCollection(this.collection)
-    		.then(function(collection) {
-    			return collection.updateOne.apply(collection, args);
-    		});
-    }
-
-    updateMany(query, update, options) {
-    	var args = arguments;
-    	return this.constructor._getCollection(this.collection)
-    		.then(function(collection) {
-    			return collection.updateMany.apply(collection, args);
-    		});
-    }
-
-    deleteOne(query, options) {
-    	var args = arguments;
-    	return this.constructor._getCollection(this.collection)
-    		.then(function(collection) {
-    			return collection.deleteOne.apply(collection, args);
-    		});
-    }
-
-    deleteMany(query, options) {
-    	var args = arguments;
-    	return this.constructor._getCollection(this.collection)
-    		.then(function(collection) {
-    			return collection.deleteMany.apply(collection, args);
-    		});
+        let store = new Container();
+        store.registerAdapter('mongodb', adapter, { 'default': true });
+        return store;
     }
 
 }
 
-module.exports = MongodbDAO;
+module.exports = MongodbDao;
